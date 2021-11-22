5631820feccfbfeee2360ddca5abca7913c56efb
Chris <chrisclayhendrik@gmail.com>
2021-11-22 14:00:26 -0600

    Updated test cases

--- pet.test.js
+++ pet.test.js
@@ -6,4 +6,10 @@ describe('Pet class', () => {
         const cat = new Pet('Mr Meowgi', 'Calico');
         expect(cat.name).toBe('Mr Meowgi');
     })
+
+    test('has type', () => {
+        const cat = new Pet('Mr Meowgi', 'Calico');
+        expect(cat.species).toBe('Calico');
+    })
 })
+
